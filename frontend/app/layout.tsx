f6301d0154c576e39ff85bc9331f84a3e7cc1211
jazzbox35 <jazzbox35@localhost.localdomain>
2025-12-21 21:41:29 -0800

    updates

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
             <footer className="border-t py-3 md:py-0">
               <div className=" flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
                 <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                  © {new Date().getFullYear()} Pattern miner tutorial. All
+                  © {new Date().getFullYear()} Chess tutorial. All
                   rights reserved.
                 </p>
                 <div className="flex items-center gap-4"></div>
