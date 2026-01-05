f129d0126c20e07bbd1cff6fc64c2ace849c98ff
jazzbox35 <jazzbox35@localhost.localdomain>
2026-01-05 04:21:05 -0800

    push for render

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -13,8 +13,8 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:5000/:path*",
-        //destination: "https://mwj-latest.onrender.com/:path*",
+        //destination: "http://127.0.0.1:5000/:path*",
+        destination: "https://mwj-latest.onrender.com/:path*",
       },
     ]
   },
